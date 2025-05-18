84f08a2de1edde5592a13200af1b109475a05fb0
조규찬 <135817111+maskDDD@users.noreply.github.com>
2025-05-18 20:20:41 +0900

    Title: 만나는 그 순간, Time: 114ms, Memory: 49MB, Status: Passed - Codetree

--- 250518/만나는 그 순간/the-moment-we-meet.py
+++ 250518/만나는 그 순간/the-moment-we-meet.py
@@ -2,11 +2,13 @@ N, M = map(int, input().split())
 Max= 2000002
 A =[int(1e9) for _ in range(Max)]
 B =[int(1e9) for _ in range(Max)]
+Time =0
 
 Acur=0
 Atime=0
 for _ in range(N):
     d, t = input().split()
+    Time +=int(t)
     sign = -1 if(d=="L") else 1
     for _ in range(int(t)):
         Acur = Acur + sign*1
@@ -23,7 +25,7 @@ for _ in range(M):
         Btime+=1
         B[Btime]= Bcur
 
-for i in range(1, Max):    # i는 시간(초), 시작 0초 제외
+for i in range(1, Time+1):    # i는 시간(초), 시작 0초 제외
     if(A[i] == B[i]):       # i초에 서로 위치가 같다면
         print(i)
         exit()

be070377783011f57f8780cb1b80abee0b7e6730
조규찬 <135817111+maskDDD@users.noreply.github.com>
2025-05-19 20:45:29 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 48ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250519/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -0,0 +1,22 @@
+def verfInfest(i, j):
+    if(infested[i] and handCount[i] !=0):
+        handCount[j] -=1
+        infested[j] = 1
+
+N,K,P,T = map(int,input().split())
+
+infested =[0 for _ in range(N+1)]
+infested[P] = 1
+handCount = [K for _ in range(N+1)]
+
+handShake=[]
+for _ in range(T):
+    handShake.append(list(map(int, input().split())))
+handShake = sorted(handShake, key = lambda x : x[0])
+
+for hs in handShake:
+    t,x,y = hs
+    verfInfest(x, y)
+    verfInfest(y, x)
+for i in infested[1:]:
+    print(i, end="")

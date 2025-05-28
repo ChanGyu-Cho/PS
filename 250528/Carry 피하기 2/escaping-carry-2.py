54bf28e74147397d61eb5e14d178a838f81a2330
조규찬 <135817111+maskDDD@users.noreply.github.com>
2025-05-28 19:36:51 +0900

    Title: Carry 피하기 2, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250528/Carry 피하기 2/escaping-carry-2.py
@@ -0,0 +1,33 @@
+n = int(input())
+arr = [int(input()) for _ in range(n)]
+
+# Please write your code here.
+maxSum =0
+
+for i in range(n):
+    for j in range(n):
+        for k in range(n):
+            if(i==j or j==k or k==i):   # 겹치는 모든 케이스 통과
+                continue
+            a,b,c = str(arr[i]), str(arr[j]), str(arr[k])
+
+            while(len(a) != 5):
+                a = "0"+a
+            while(len(b) != 5):
+                b = "0"+b
+            while(len(c) != 5):
+                c = "0"+c
+
+            passFlag = True
+            for x,y,z in zip(a,b,c):
+                s = int(x)+int(y)+int(z)
+                if(s>=10):
+                    passFlag = False
+                    break
+
+            if(passFlag):
+                S = int(a)+int(b)+int(c)
+                maxSum =max(maxSum, S)
+print(maxSum)
+
+

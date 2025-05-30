12277821ccfcb0a41dfa736713572ffd43cfaf69
조규찬 <135817111+maskDDD@users.noreply.github.com>
2025-05-30 19:44:43 +0900

    Title: 오목, Time: 42ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250530/오목/O-mok.py
+++ 250530/오목/O-mok.py
@@ -2,20 +2,20 @@ board = [list(map(int, input().split())) for _ in range(19)]
 
 # Please write your code here.
 N = 19
-
-def search(cor, i,j):
-    horFlag, verFlag, diaFlag = True, True, True
-    #가로
+    
+def ver(cor, i,j):
+    verFlag = True
     for x in range(i,i+5):
-        if(board[x][j] != cor):
-            horFlag = False
+        if(board[x][j] !=cor):
+            verFlag = False
             break
-    if(horFlag):
+    if(verFlag):
         print(cor)
         print(i+3, j+1)
         exit(0)
-        return
-    # 대각
+
+def dia(cor,i,j):
+    diaFlag = True
     for x,y in zip(range(i,i+5), range(j,j+5)):
         if(board[x][y] !=cor):
             diaFlag = False
@@ -24,21 +24,26 @@ def search(cor, i,j):
         print(cor)
         print(i+3, j+3)
         exit(0)
-        return
-    # 세로
+
+def hor(cor,i,j):
+    horFlag = True
     for y in range(j,j+5):
-        if(board[i][y] !=cor):
-            verFlag = False
+        if(board[i][y] != cor):
+            horFlag = False
             break
-    if(verFlag):
+    if(horFlag):
         print(cor)
         print(i+1, j+3)
         exit(0)
-        return
 
-
-for i in range(N-4):
-    for j in range(N-4):
-        if not board[i][j]==0:
-            search(board[i][j],i,j)
+for i in range(N):
+    for j in range(N):
+        cor = board[i][j]
+        if not cor==0:
+            if j<15:
+                hor(cor,i,j)
+            if i<15 and j<15:
+                dia(cor, i,j)
+            if i<15:
+                ver(cor,i,j)
 print(0)

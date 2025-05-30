bfa2b82cfcf6f19385d15d48664782540e2908ff
조규찬 <135817111+maskDDD@users.noreply.github.com>
2025-05-30 19:55:26 +0900

    Title: 오목, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250530/오목/O-mok.py
+++ 250530/오목/O-mok.py
@@ -14,15 +14,26 @@ def ver(cor, i,j):
         print(i+3, j+1)
         exit(0)
 
-def dia(cor,i,j):
-    diaFlag = True
+def dia1(cor,i,j):
+    dia1Flag = True
     for x,y in zip(range(i,i+5), range(j,j+5)):
         if(board[x][y] !=cor):
-            diaFlag = False
+            dia1Flag = False
             break
-    if(diaFlag):
+    if(dia1Flag):
         print(cor)
         print(i+3, j+3)
+        exit(0)
+
+def dia2(cor,i,j):
+    dia2Flag = True
+    for x,y in zip(range(i,i+5), range(j, j-5, -1)):
+        if(board[x][y] !=cor):
+            dia2Flag = False
+            break
+    if(dia2Flag):
+        print(cor)
+        print(i+3, j-1)
         exit(0)
 
 def hor(cor,i,j):
@@ -43,7 +54,9 @@ for i in range(N):
             if j<15:
                 hor(cor,i,j)
             if i<15 and j<15:
-                dia(cor, i,j)
+                dia1(cor, i,j)
             if i<15:
                 ver(cor,i,j)
+            if j>3 and i <15:
+                dia2(cor,i,j)
 print(0)

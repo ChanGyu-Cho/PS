2bdd8846e3e6298507500320a975089733d91272
조규찬 <135817111+maskDDD@users.noreply.github.com>
2025-06-01 17:09:06 +0900

    Title: 스승의 은혜 2, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250601/스승의 은혜 2/the-grace-form-teacher-2.py
+++ 250601/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -3,30 +3,21 @@ P = [int(input()) for _ in range(N)]
 
 # Please write your code here.
 
-maxDep = 0
-visited = [False for _ in range(N)]
+maxCount = 0
+P.sort()
 
-def selStu(dep, SUM):
-    global maxDep
-    if(dep == N+1):
-        return
-    if(SUM >= B):
-        return
-    maxDep = max(maxDep, dep)
-    if(maxDep == N):
-        return
+for halfStu in range(N):   # 학생 하나씩 반값 선택
+    SUM = P[halfStu]//2
+    if(SUM >=B):
+        break
+    count =1
     for stu in range(N):
-        if(not visited[stu]):
-            visited[stu] = True
-            selStu(dep+1, SUM+P[stu])
-            visited[stu] = False
-
-for stu in range(N):   # 학생 하나씩 반값 선택
-    visited[stu] = True
-    SUM = P[stu]//2
-    if(SUM > B):
-        continue
-    maxDep = 1
-    selStu(2, SUM) # 깊이 1로 재귀
-    visited[stu] = False
-print(maxDep)
+        if(halfStu == stu):
+            continue
+        SUM += P[stu]
+        if(SUM >=B):    # 제한 넘기면, 어차피 다음은 더큰 선물이기에 파기
+            break
+        count +=1
+    maxCount = max(maxCount, count)
+    
+print(maxCount)

6ae31f61368519585cc142b792d8ae6745d475dd
조규찬 <135817111+maskDDD@users.noreply.github.com>
2025-06-01 17:17:41 +0900

    Title: 스승의 은혜 3, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250601/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 250601/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -1,5 +1,6 @@
 N, B = map(int, input().split())
 gifts = [tuple(map(int, input().split())) for _ in range(N)]
+gifts = sorted(gifts, key=lambda x: x[0]+x[1])
 P = [gift[0] for gift in gifts]
 S = [gift[1] for gift in gifts]
 """
@@ -8,24 +9,21 @@ S = [gift[1] for gift in gifts]
 """
 # Please write your code here.
 
-maxDep = 0
-visited = [False for _ in range(N)]
 
-def selStu(dep, SUM):
-    global maxDep
-    if(dep == N):
-        return
-    if(SUM > B):
-        return
-    maxDep = max(maxDep, dep)
+maxCount = 0
+
+for halfStu in range(N):   # 학생 하나씩 반값 선택
+    SUM = P[halfStu]//2 +S[halfStu]
+    if(SUM >=B):
+        break
+    count =1
     for stu in range(N):
-        if(not visited[stu]):
-            visited[stu] = True
-            selStu(dep+1, SUM+P[stu]+S[stu])
-            visited[stu] = False
-
-for stu in range(N):   # 학생 하나씩 반값 선택
-    visited[stu] = True
-    selStu(1, P[stu]//2+S[stu]) # 깊이 1로 재귀
-    visited[stu] = False
-print(maxDep)
+        if(halfStu == stu):
+            continue
+        SUM += P[stu] +S[stu]
+        if(SUM >B):    # 제한 넘기면, 어차피 다음은 더큰 선물이기에 파기
+            break
+        count +=1
+    maxCount = max(maxCount, count)
+    
+print(maxCount)
